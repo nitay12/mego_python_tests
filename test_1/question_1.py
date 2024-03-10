78105afe807049cdaa217538b851f2e1d07df605
Nitay Caspi <NitayCaspi12@gmail.com>
2024-03-10 12:38:27 +0200

    Test 1 Q1 V2

--- test_1/question_1.py
+++ test_1/question_1.py
@@ -4,17 +4,33 @@
 # 2. Calculate and print the average value of all the given numbers
 
 # Solution v1:
+# numbers_sum = 0
+# numbers_count = 0
+# while True:
+#     input_number = int(input("Enter a whole number (or 500 to stop): "))
+#     if input_number == 500:
+#         print("Average of all numbers: ", (numbers_sum / numbers_count))
+#         break
+#     numbers_sum += input_number
+#     numbers_count += 1
+#     if input_number %2 != 0:
+#         sum = 0
+#         for digit in str(input_number):
+#             sum += int(digit)
+#         print("Digits sum: ", sum)
+
+# Solution v2
+input_number = 0
 numbers_sum = 0
-numbers_count = 0
-while True:
+arr = []
+while input_number != 500:
     input_number = int(input("Enter a whole number (or 500 to stop): "))
-    if input_number == 500:
-        print("Average of all numbers: ", (numbers_sum / numbers_count))
-        break
+    arr.append(input_number)
     numbers_sum += input_number
-    numbers_count += 1
-    if input_number %2 != 0:
-        sum = 0
-        for digit in str(input_number):
-            sum += int(digit)
-        print("Digits sum: ", sum)
+    odd_sum = 0
+    if input_number%2 != 0:
+        while input_number != 0:
+            odd_sum += input_number%10
+            input_number = input_number//10
+        print(odd_sum)
+print(numbers_sum/len(arr))
