f1372310727f61fe5d23a418cba39bfb48bfb18c
Nitay Caspi <NitayCaspi12@gmail.com>
2024-07-03 16:36:26 +0300

    add another version

--- various_queestions/summer_2021_A/summer_2021_Q_9_A.py
+++ various_queestions/summer_2021_A/summer_2021_Q_9_A.py
@@ -1,4 +1,4 @@
-# A
+# A v1
 def exchange(number):
     while number > 0:
         if number < 10:
@@ -7,8 +7,17 @@ def exchange(number):
             return False
         number //= 10
     return True
+# A v2
+def exchange(num):
+    if num < 10:
+        return True
+    string = str(num)
+    for i in range(len(string)-1):
+        if int(string[i]) % 2 == int(string[i + 1]) % 2:
+            return False
+    return True
 
-
+# B v1
 def min_exchange_d_sum(arr): 
     def d_sum(num):
         sum = 0
@@ -25,6 +34,19 @@ def min_exchange_d_sum(arr):
             if dsum < min_sum or min_sum == -1:
                 index = i
                 min_sum = dsum
+    return index
+# B v2
+def min_exchange_d_sum(arr):
+    min_dsum = float("inf")
+    index = -1
+    for i in range(len(arr)):
+        if exchange(arr[i]):
+            d_sum = 0
+            for digit in str(arr[i]):
+                d_sum += int(digit)
+            if d_sum < min_dsum:
+                min_dsum = d_sum
+                index = i
     return index
 
 print(min_exchange_d_sum([16, 33, 14, 567]))
